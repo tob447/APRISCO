fa2de4bdcfd091baa58746c82a2b12df1528b3ea
Camila White <camilawhite98@gmail.com>
2019-08-24 10:55:34 -0500

    Add files via upload

--- APRISCO/src/app/app-routing.module.ts
+++ APRISCO/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { AllPlantsComponent } from './all-plants/all-plants.component';
+//import { HomeComponent } from './home/home.component';
+//import { AllPlantsComponent } from './all-plants/all-plants.component';
 
 const routes: Routes = [
   //{path: '', component: HomeComponent},
